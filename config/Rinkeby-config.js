7a0eaada93b6955934c1dd72b37d85794ba7e508
KoNit-DeepGo <konit2021@gmail.com>
2021-08-03 12:00:40 +0800

    Add task for setUpgrade the NudgePool
    Signed-off-by: KoNit-DeepGo <konit2021@gmail.com>

--- config/Rinkeby-config.js
+++ config/Rinkeby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
     RINKEBY_CONFIG: {
         NudgePool: "0xFA6681461FB88b7084994200b460dE461F5BdD39",
+
+        newVersion: "0.0.2",
+        newIPLogic: "0x7058663297e7667480486602913b42102274691b",
+        newGPDepositLogic: "0x5Daed5D252405A84C5bA06978F9Ce8109828389c",
+        newGPWithdrawLogic: "0xbc1dAB19704a0057ef045940817a193D64f3478E",
+        newLPLogic: "0x85CE594f64119AD8baB9F3a4D2E48258dCed3F40",
+        newVaultLogic: "0x30B9B2F87C2405F3E5c1A9E574B4B98A9c86F0ce",
+        newStateLogic: "0x23DF2C423E6200d05a6ED9f42242ff2B07302d9a",
+        newLiquidationLogic: "0x4dA5638b050459C9C02C32EA298E3299a1B6b0dA",
     }
 }
--- task/auto-compile.ts
+++ task/auto-compile.ts
@@ -27,3 +27,20 @@ task("unpause", "Unpasue all the external NudgePool functions").setAction(async 
         console.log("NudgePool successful unpaused");
     });
 });
+
+task("set-upgrade", "Set up a new upgrade for logic contract address").setAction(async (_, { ethers }) => {
+    const NudgePoolAddress = RINKEBY_CONFIG.NudgePool;
+    const NudgePool = await ethers.getContractAt('NudgePool', NudgePoolAddress);
+    await NudgePool.setUpgrade(
+        RINKEBY_CONFIG.newVersion,
+        RINKEBY_CONFIG.newIPLogic,
+        RINKEBY_CONFIG.newGPDepositLogic,
+        RINKEBY_CONFIG.newGPWithdrawLogic,
+        RINKEBY_CONFIG.newLPLogic,
+        RINKEBY_CONFIG.newVaultLogic,
+        RINKEBY_CONFIG.newStateLogic,
+        RINKEBY_CONFIG.newLiquidationLogic
+    ).then(() => {
+        console.log("Successful set up a new upgrade");
+    });
+});
