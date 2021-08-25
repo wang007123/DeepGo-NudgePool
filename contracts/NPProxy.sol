27d45842762b107244d2d1d2d83ac0c7b1785150
Andrew-DeepGo <andrew.fang@deepquant.group>
2021-08-25 11:30:24 +0800

    Add check if lacking of input validation
    Signed-off-by: Andrew-DeepGo <andrew.fang@deepquant.group>

--- contracts/NPProxy.sol
+++ contracts/NPProxy.sol
@@ -50,6 +50,7 @@ contract NPProxy is Ownable {
                 _lpc != address(0) && _vtc != address(0) && _stc != address(0) &&
                 _lqdc != address(0), "Wrong Address");
         require(bytes(_newVersion).length > 0, "Empty Version");
+        require(keccak256(abi.encodePacked(versionName)) != keccak256(abi.encodePacked(_newVersion)), "Existing Version");
         delayVersionName = _newVersion;
         delayVersion.ipc = _ipc;
         delayVersion.gpdc = _gpdc;
